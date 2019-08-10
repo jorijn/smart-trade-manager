71090e66774ad6d6574ed11c176b5266e29d579c
Jorijn Schrijvershof <jorijn@jorijn.com>
2019-08-10 08:54:06 +0200

    added http client

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,29 @@
-const Encore = require('@symfony/webpack-encore');
-const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+const Encore = require("@symfony/webpack-encore");
+const VuetifyLoaderPlugin = require("vuetify-loader/lib/plugin");
 
 if (!Encore.isRuntimeEnvironmentConfigured()) {
-    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || "dev");
 }
 
-Encore
-    .setOutputPath('public/build/')
-    .setPublicPath('/build')
-    .addEntry('app', './assets/js/app.js')
-    .splitEntryChunks()
-    .enableSingleRuntimeChunk()
-    .cleanupOutputBeforeBuild()
-    .enableBuildNotifications()
-    .enableSourceMaps(!Encore.isProduction())
-    .enableVersioning(Encore.isProduction())
-    .configureBabel(() => {}, {
-        useBuiltIns: 'usage',
-        corejs: 3
-    })
-    .enableIntegrityHashes(Encore.isProduction())
-    .enableVueLoader()
-    .addPlugin(new VuetifyLoaderPlugin())
-    .enableSassLoader(options => {
-        options.implementation = require('sass');
-        options.fiber = require('fibers');
-    })
-;
+Encore.setOutputPath("public/build/")
+  .setPublicPath("/build")
+  .addEntry("app", "./assets/js/app.js")
+  .splitEntryChunks()
+  .enableSingleRuntimeChunk()
+  .cleanupOutputBeforeBuild()
+  .enableBuildNotifications()
+  .enableSourceMaps(!Encore.isProduction())
+  .enableVersioning(Encore.isProduction())
+  .configureBabel(() => {}, {
+    useBuiltIns: "usage",
+    corejs: 3
+  })
+  .enableIntegrityHashes(Encore.isProduction())
+  .enableVueLoader()
+  .addPlugin(new VuetifyLoaderPlugin())
+  .enableSassLoader(options => {
+    options.implementation = require("sass");
+    options.fiber = require("fibers");
+  });
 
 module.exports = Encore.getWebpackConfig();
