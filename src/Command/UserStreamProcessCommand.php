d24d06be4c5c489fe183d2e9a3b9918246d647dc
Jorijn Schrijvershof <jorijn@jorijn.com>
2019-09-22 09:56:01 +0200

    changed websocket component, ensures more robust handling of packages

--- src/Command/UserStreamProcessCommand.php
+++ src/Command/UserStreamProcessCommand.php
@@ -2,19 +2,24 @@
 
 namespace App\Command;
 
-use Amp\Loop;
-use Amp\Websocket;
 use App\Bus\Message\Command\SynchronizeOrderHistoryCommand;
 use App\Bus\Message\Event\WebsocketEvent;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
 use Psr\Log\LoggerInterface;
+use Ratchet\Client\Connector;
+use Ratchet\Client\WebSocket;
+use Ratchet\RFC6455\Messaging\MessageInterface;
+use React\EventLoop\LoopInterface;
+use React\Socket\Connector as ReactConnector;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Throwable;
 
 class UserStreamProcessCommand extends Command implements LoggerAwareInterface
 {
@@ -27,12 +32,18 @@ class UserStreamProcessCommand extends Command implements LoggerAwareInterface
     protected $accessor;
     /** @var string */
     protected $listenKey;
-    /** @var Websocket\Connection */
-    protected $connection;
+    /** @var WebSocket */
+    protected $websocket;
     /** @var MessageBusInterface */
     protected $eventBus;
     /** @var MessageBusInterface */
     protected $commandBus;
+    /** @var LoopInterface */
+    protected $loop;
+    /** @var string */
+    protected $dns;
+    /** @var int */
+    protected $timeout;
 
     /**
      * @param string                    $name
@@ -41,6 +52,9 @@ class UserStreamProcessCommand extends Command implements LoggerAwareInterface
      * @param MessageBusInterface       $eventBus
      * @param MessageBusInterface       $commandBus
      * @param LoggerInterface           $logger
+     * @param LoopInterface             $loop
+     * @param string                    $dns
+     * @param int                       $timeout
      */
     public function __construct(
         string $name,
@@ -48,13 +62,19 @@ class UserStreamProcessCommand extends Command implements LoggerAwareInterface
         PropertyAccessorInterface $accessor,
         MessageBusInterface $eventBus,
         MessageBusInterface $commandBus,
-        LoggerInterface $logger
+        LoggerInterface $logger,
+        LoopInterface $loop,
+        string $dns = '8.8.8.8',
+        int $timeout = 10
     ) {
         parent::__construct($name);
         $this->httpClient = $binanceApiClient;
         $this->accessor = $accessor;
         $this->eventBus = $eventBus;
         $this->commandBus = $commandBus;
+        $this->loop = $loop;
+        $this->dns = $dns;
+        $this->timeout = $timeout;
 
         $this->setLogger($logger);
     }
@@ -63,19 +83,11 @@ class UserStreamProcessCommand extends Command implements LoggerAwareInterface
     {
         $this->logger->info('Received kill signal');
 
-        if ($this->connection) {
+        if ($this->websocket) {
             // if we get here, close the websocket connection
             $this->logger->info('Closing websocket', ['listenKey' => $this->listenKey]);
 
-            $this->connection->close();
-            $this->httpClient->request(
-                'DELETE',
-                'v1/userDataStream',
-                [
-                    'extra' => ['security_type' => 'USER_STREAM'],
-                    'body' => ['listenKey' => $this->listenKey],
-                ]
-            );
+            $this->websocket->close();
         }
     }
 
@@ -97,55 +109,48 @@ class UserStreamProcessCommand extends Command implements LoggerAwareInterface
 
         $this->listenKey = $this->accessor->getValue($response->toArray(false), '[listenKey]');
 
-        // start the loop
-        $this->logger->info('Starting websocket', ['listenKey' => $this->listenKey]);
-
-        try {
-            $this->buildAndProcessWebsocket();
-        } catch (Websocket\ClosedException $exception) {
-            $this->logger->info('Closed websocket', [
-                'reason' => $exception->getReason(),
-            ]);
-        }
+        $this->createWebsocket();
 
         return 0;
     }
 
-    protected function buildAndProcessWebsocket()
+    protected function createWebsocket()
     {
-        $lastPing = time();
-        Loop::run(function () use (&$lastPing) {
-            /* @var Websocket\Connection $connection */
-            $this->connection = yield Websocket\connect('wss://stream.binance.com:9443/ws/'.$this->listenKey);
-            $this->logger->info('Websocket connected', ['listenKey' => $this->listenKey]);
+        $reactConnector = new ReactConnector($this->loop, [
+            'dns' => $this->dns,
+            'timeout' => $this->timeout,
+        ]);
 
-            /** @var Websocket\Message $message */
-            while ($message = yield $this->connection->receive()) {
-                pcntl_signal_dispatch();
+        $connector = new Connector($this->loop, $reactConnector);
 
-                $payload = yield $message->buffer();
-                $this->handlePayload($payload);
+        $this->logger->info('Starting websocket', ['listenKey' => $this->listenKey]);
+        $connector('wss://stream.binance.com:9443/ws/'.$this->listenKey, [], [])
+            ->then(function (WebSocket $webSocket) {
+                $this->websocket = $webSocket;
+                $this->logger->info('Websocket connected', ['listenKey' => $this->listenKey]);
 
-                // if the last ping was 30 minutes ago, send another.
-                if ($lastPing < (time() - 1800)) {
-                    $lastPing = time();
+                $webSocket->on('message', function (MessageInterface $message) {
+                    $this->onWebsocketPayload((string) $message);
+                });
 
-                    // this signals the API to keep the websocket open
-                    $this->logger->info('Sending ping for websocket', ['listenKey' => $this->listenKey]);
-                    $this->httpClient->request(
-                        'PUT',
-                        'v1/userDataStream',
-                        [
-                            'extra' => ['security_type' => 'USER_STREAM'],
-                            'body' => ['listenKey' => $this->listenKey],
-                        ]
-                    );
-                }
-            }
-        });
+                $webSocket->on('close', function ($code = null, $reason = null) {
+                    $this->onWebsocketClose($code, $reason);
+                });
+            })
+            ->otherwise(function (Throwable $exception) {
+                $this->logger->error('Websocket could not connect: {reason}', [
+                    'exception' => $exception,
+                    'reason' => $exception->getMessage() ?: get_class($exception),
+                ]);
+            });
+
+        $this->loop->run();
+
+        $this->registerKeyRenewalTimer();
+        $this->registerSignalDispatcher();
     }
 
-    protected function handlePayload(string $payload): void
+    protected function onWebsocketPayload(string $payload): void
     {
         $data = json_decode($payload, true);
         $event = new WebsocketEvent($data['e'], $data);
@@ -153,5 +158,50 @@ class UserStreamProcessCommand extends Command implements LoggerAwareInterface
         $this->logger->info('Dispatching websocket event', ['event' => $data['e'], 'payload' => $data]);
 
         $this->eventBus->dispatch($event);
+    }
+
+    /**
+     * @param mixed $code
+     * @param mixed $reason
+     *
+     * @throws TransportExceptionInterface
+     */
+    protected function onWebsocketClose($code, $reason)
+    {
+        $this->httpClient->request(
+            'DELETE',
+            'v1/userDataStream',
+            [
+                'extra' => ['security_type' => 'USER_STREAM'],
+                'body' => ['listenKey' => $this->listenKey],
+            ]
+        );
+
+        $this->logger->info(
+            'Websocket closed',
+            ['listenKey' => $this->listenKey, 'code' => $code, 'reason' => $reason]
+        );
+    }
+
+    protected function registerKeyRenewalTimer(): void
+    {
+        $this->loop->addPeriodicTimer(1800, function () {
+            $this->logger->info('Sending ping for websocket', ['listenKey' => $this->listenKey]);
+            $this->httpClient->request(
+                'PUT',
+                'v1/userDataStream',
+                [
+                    'extra' => ['security_type' => 'USER_STREAM'],
+                    'body' => ['listenKey' => $this->listenKey],
+                ]
+            );
+        });
+    }
+
+    protected function registerSignalDispatcher(): void
+    {
+        $this->loop->addPeriodicTimer(1, static function () {
+            pcntl_signal_dispatch();
+        });
     }
 }
